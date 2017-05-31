230ae76f97bc3719a3e995fc5b6360490b1765d0
mandisimakwakwa <mandisi.makwakwa@gmail.com>
2017-06-01 01:50:19 +0200

    - Implemented Controllers - Implemented Includes
    Signed-off-by: mandisimakwakwa <mandisi.makwakwa@gmail.com>

--- sources/frontEnd/html/htmlTemplates/header.php
+++ sources/frontEnd/html/htmlTemplates/header.php
@@ -7,9 +7,9 @@
         <?php
         /*            if ($pageTypeCheck == 'mainPage') {
 
-                        echo "Welcome to Camagru: ";
-                        echo ucfirst($dbUsername);
-                    } elseif ($pageTypeCheck == 'indexPage') {
+                                echo "Welcome to Camagru: ";
+                                echo ucfirst($dbUsername);
+                            } elseif ($pageTypeCheck == 'indexPage') {
 
                         echo "Welcome to Camagru";
                     }
