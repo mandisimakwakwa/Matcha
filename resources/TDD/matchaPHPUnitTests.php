021af8aa408b7cc945925897c64934775f9e8aeb
mandisimakwakwa <mandisi.makwakwa@gmail.com>
2017-09-05 17:01:54 +0200

    Unit Test Blocker Complete

--- resources/TDD/matchaPHPUnitTests.php
+++ resources/TDD/matchaPHPUnitTests.php
@@ -4,28 +4,72 @@
 $projectRoot = substr(getcwd(), 0, strpos(getcwd(), "sources"));
 require $projectRoot . 'sources/backEnd/engines/controllers/relativePathController.php';
 
-    function ft_runUnitTests() {
+//PHP Custom Rudimentary Unit Tests
 
-        assert_options(ASSERT_ACTIVE, 1);
+    //Unit Tests
+    function ft_runUnitTests() {
 
         try {
 
+            $counter = 0;
+
+            //Test Sum
+            $testSum = 2;
+
             //Test One
 
-            $expectedResult = "indes    x";
+            $testNumber = 1;
+
+            $functionName = "ft_getCurrentFilename";
+            $functionLocation = "matchaDTO.php";
+            $functionStartLine = "Line 7";
+
             $originalResult = ft_getCurrentFilename();
+            $expectedResult = "index";
 
             if ($expectedResult == $originalResult) {
 
-                return true;
-            } else{
+                $counter += 1;
+            } else {
 
-                throw New Exception('MyException1');
+                throw New Exception(ft_unitTestFailError($testNumber, $functionName, $functionLocation, $functionStartLine));
+            }
+
+            //Test Two
+
+            $testNumber = 2;
+
+            $functionName = "ft_checkPage";
+            $functionLocation = "matchaDTO.php";
+            $functionStartLine = "Line 15";
+
+            $originalResult = ft_checkPage("main");
+            $expectedResult = "mains";
+
+            if ($expectedResult == $originalResult) {
+
+                $counter += 1;
+            } else {
+
+                throw New Exception(ft_unitTestFailError($testNumber, $functionName, $functionLocation, $functionStartLine));
+            }
+
+            if ($testSum == $counter) {
+
+                return true;
             }
 
         } catch (Exception $e) {
 
             echo $e->getMessage();
         }
+    }
+
+    //Unit Test Failure
+    function ft_unitTestFailError($testNumber, $functionName, $functionLocation, $functionStartLine) {
+
+        $errorMessage = "Error: Test $testNumber within $functionLocation on line $functionStartLine, $functionName.";
+
+        return $errorMessage;
     }
 ?>
